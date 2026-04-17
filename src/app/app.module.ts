71842e3e6defe6eebf79e32786e37726baffb24d
sagaredatta5-glitch <sagaredatta5@gmail.com>
2026-04-17 09:39:12 +0530

    TodoCRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import{MatButtonModule} from '@angular/material/button';
+import{MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,85 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Itodo } from '../models/todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent implements OnInit {
+   todosArr: Array<Itodo> = [
+    {
+      todoItem:'Js',
+      todoId:'123'
+   },
+   {
+      todoItem:'Ts',
+      todoId:'124'
+   },
+   {
+      todoItem:'DS',
+      todoId:'125'
+   }
+  ]
+  isInEditMode : boolean = false;
+  editId !: string
+  @ViewChild('todoItem') todoItem !: ElementRef
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  onTodoAdd(){
+    //get Todo obj
+    let todoobj : Itodo ={
+      todoItem : this.todoItem.nativeElement.value,
+      todoId : Date.now().toString()
+    }
+    this.todoItem.nativeElement.value =''
+    
+
+    //push arr
+    this.todosArr.push(todoobj)
+
+    //create a new li
+
+  }
+
+ trackById(index: number,todo : Itodo){
+  return todo.todoId
+ } 
+
+ removeTodo(id : string){
+
+  let getIndex = this.todosArr.findIndex(t=> t.todoId===id)
+  this.todosArr.splice(getIndex,1)
+ }
+ 
+  onEdit(todo : Itodo){
+    console.log(todo);
+    this.editId = todo.todoId
+    this.todoItem.nativeElement.value = todo.todoItem;
+    this.isInEditMode = true;
+  }
+
+  onUpdate(){
+    //UPDATE TODO
+    let UPDATED_TODO : Itodo ={
+      todoItem : this.todoItem.nativeElement.value,
+      todoId : this.editId
+    }
+    console.log(UPDATED_TODO);
+    
+    this.todoItem.nativeElement.value =''
+    //ARRAY me replace
+
+    let getIndex = this.todosArr.findIndex(t => t.todoId===UPDATED_TODO.todoId)
+    this.todosArr[getIndex] =UPDATED_TODO
+    this.isInEditMode = false
+  }
+
+}
+
+
+//zone.js >> changeDetection.default
+//zone.js >> changeDetection.onpush
